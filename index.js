dc6554823295b2cae011116a011bd3bfa89061c8
Inamov Asilbek <playerallgames007@gmail.cpm>
2024-04-25 17:50:59 +0500

    TG_Bot

---
+++ index.js
@@ -0,0 +1,21 @@
+const TelegramBot = require('node-telegram-bot-api');
+const { start } = require('repl');
+
+const token = '7100657594:AAHg_ScSFYUi2WsKLLzjyDSi12g-eMayD_I';
+
+const bot = new TelegramBot(token, { polling: true });
+
+// 
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id
+    console.log(msg.chat.first_name + " " + msg.chat.last_name);
+    if (msg.text === "/start") {
+        bot.sendMessage(chatId, "Assalomu alaykum " + msg.chat.first_name + " " + msg.chat.last_name + " 😇!!! 1nm0py ni botiga xushkelibsiz ")
+    } else if (msg.text === "/help") {
+        bot.sendMessage(chatId, "Assalomu alaykum. Qanday yordam bera olaman??? ")
+    }
+    else {
+        bot.sendMessage(chatId, "Assalomu alaykum. Xato ma'lumot!!! ")
+    }
+});
